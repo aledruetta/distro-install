1c9413eae3d5ce6cbf4ca83433ccc9cd3bc6d52c
Alejandro Druetta <aledruetta@gmail.com>
2017-02-21 00:47:29 -0300

    ascii art, hardware info, checklist

--- mint17-install.sh
+++ mint17-install.sh
@@ -1,15 +1,40 @@
 #!/usr/bin/env bash
+#  ______   _________  ______   ______
+# /_____/\ /________/\/_____/\ /_____/\
+# \::::_\/_\__.::.__\/\::::_\/_\:::__\/
+#  \:\/___/\  \::\ \   \:\/___/\\:\ \  __
+#   \::___\/_  \::\ \   \::___\/_\:\ \/_/\
+#    \:\____/\  \::\ \   \:\____/\\:\_\ \ \
+#     \_____\/   \__\/    \_____\/ \_____\/
+#
+# Instalador de Linux Mint 17.3 computadores antigos do laboratório de
+# informática da ETEC de Ubatuba, SP.
+#
+# Hardware:
+# CPU Pentium 4, 1GB RAM, 40GB HD.
+#
+# Autores: Alejandro e Lucas.
+#
 
-echo '### Update & Upgrade ###'
+echo '##########################################'
+echo '############ Update & Upgrade ############'
+echo '##########################################'
+
 echo 'Estamos atualizando o sistema. Isso vai demorar, paciência...'
 sudo apt-get update
 sudo apt-get upgrade -y
 
-echo '### Codecs ###'
+echo '################################'
+echo '############ Codecs ############'
+echo '################################'
+
 echo 'Estamos instalando alguns codecs adicionais e as ferramentas de compilação...'
 sudo apt-get install ubuntu-restricted-extras build-essential -y
 
-echo '### Java 8 SDK ###'
+echo '####################################'
+echo '############ Java 8 SDK ############'
+echo '####################################'
+
 echo 'Atualizando o Java SDK pra versão 8 via PPA...'
 sudo apt-add-repository ppa:openjdk-r/ppa -y
 sudo apt-get update
@@ -21,25 +46,41 @@ echo 'Verifique se a versão é a correta:'
 java -version
 javac -version
 
-echo '### Netbeans 8.1 ###'
+echo '######################################'
+echo '############ Netbeans 8.1 ############'
+echo '######################################'
+
 echo 'Atualizando o Netbeans pra versão 8.1 via PPA...'
 sudo add-apt-repository ppa:vajdics/netbeans-installer -y
 sudo apt-get update
 sudo apt-get install netbeans-installer -y
 
-echo '### Sublime Text 3 ###'
+echo '########################################'
+echo '############ Sublime Text 3 ############'
+echo '########################################'
+
 echo 'Instalando o Sublime Text via PPA...'
 sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
 sudo apt-get update
 sudo apt-get install sublime-text -y
 
-echo '### Install Apps ###'
+echo '######################################'
+echo '############ Install Apps ############'
+echo '######################################'
+
 echo 'Instalando Inkscape, Dia, Meld e Workbench...'
 sudo apt-get install inkscape dia meld mysql-workbench pyrenamer -y
 
-echo '### Install LAMP ###'
+echo '######################################'
+echo '############ Install LAMP ############'
+echo '######################################'
+
 echo 'Vamos começar a instalação e configuração do LAMP Stack...'
-echo '### Apache ###'
+
+echo '################################'
+echo '############ Apache ############'
+echo '################################'
+
 sudo apt-get install apache2 -y
 sudo chown -R $USER.www-data /var/www/html
 sudo chmod -R 775 /var/www/html
@@ -47,13 +88,19 @@ ln -s /var/www/html ~
 echo 'Testando o acesso ao servidor Apache...'
 firefox http://localhost/ &
 
-echo '### PHP ###'
+echo '#############################'
+echo '############ PHP ############'
+echo '#############################'
+
 sudo apt-get install php5 libapache2-mod-php5 -y
 echo '<?php phpinfo(); ?>' > ~/html/testphp.php
 echo 'Testando o PHP...'
 firefox http://localhost/testphp.php &
 
-echo '### MySQL ###'
+echo '###############################'
+echo '############ MySQL ############'
+echo '###############################'
+
 echo 'Senha usuário root: "root"'
 sudo apt-get install mysql-server -y
 sudo apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin -y
@@ -63,7 +110,10 @@ echo 'Testando o MySQL. Para sair digite "exit"...'
 mysql -u root
 # mysql> SET PASSWORD FOR 'root'@'localhost' = PASSWORD('yourpassword');
 
-echo '### phpMyAdmin ###'
+echo '####################################'
+echo '############ phpMyAdmin ############'
+echo '####################################'
+
 echo 'Acesse o phpMyAdmin como root e mude para acessar sem password'
 sudo sed -i -E '/^\s*(\/){2}\s*.*AllowNoPassword/s/^\s*(\/){2}\s*//' /etc/phpmyadmin/config.inc.php
 sudo echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
@@ -74,22 +124,30 @@ firefox http://localhost/phpmyadmin &
 echo 'Deletando o arquivo de teste testphp.php...'
 sudo rm ~/html/testphp.php
 
-echo '### Conta "aluno" ###'
+echo '#######################################'
+echo '############ Conta "aluno" ############'
+echo '#######################################'
+
 echo 'Criando conta aluno...'
 echo 'password: "aluno". Ignorar as outras opções.'
 sudo adduser aluno
 echo 'Adicionando o usuário aluno ao grupo www-data...'
 sudo adduser aluno www-data
 groups aluno
-echo 'Criando link simbólico na home -> /var/www/html'
+echo 'Criando link simbólico na home -> /var/www/html...'
 sudo ln -s /var/www/html /home/aluno
+echo 'Salvando informação de hardware na home...'
+sudo inxi -F > ~/hardinfo.txt
 
-###### CHECKLIST #######
-# - Versão do Java
-# - Versão e funcionamento do Netbeans
-# - cat /etc/phpmyadmin/config.inc.php
-# - cat /etc/apache2/apache2.conf
-# - cat /etc/php5/apache2/php.ini
-# - Verificar Inkscape, Meld, Sublime, Dia, PyRenamer, Workbench
-# - Conta Aluno e link simbólico
-# - Google search (Firefox)
+echo '####################################'
+echo '############ CHECKLIST #############'
+echo '####################################'
+echo ''
+echo '- Versão do Java'
+echo '- Versão e funcionamento do Netbeans. Plugins HTML e PHP.'
+echo '- Verificar Inkscape, Meld, Sublime, Dia, PyRenamer, Workbench'
+echo '- cat /etc/phpmyadmin/config.inc.php'
+echo '- cat /etc/apache2/apache2.conf'
+echo '- cat /etc/php5/apache2/php.ini'
+echo '- Conta Aluno e link simbólico'
+echo '- Google search engine (Firefox)'
