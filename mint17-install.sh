d429ae2c4f9afd76af16e51e29a947f7007ea026
Alejandro Druetta <aledruetta@gmail.com>
2017-02-22 17:00:15 -0300

    PPAs e condicionais

--- mint17-install.sh
+++ mint17-install.sh
@@ -24,21 +24,24 @@
 #
 
 
-
-# Constantes
-
+# Constantes 
 readonly DESCRIPTION="`lsb_release -ds`"
 readonly CODENAME=`lsb_release -cs`
 readonly DISTRIBUTOR=`lsb_release -is`
-readonly ARQPROC=`getconf LONG_BIT`
-readonly JDKPPA="/etc/apt/sources.list.d/openjdk-r-ppa-trusty.list"
+readonly ARQ_PROC=`getconf LONG_BIT`
+readonly JDK_PPA_F="/etc/apt/sources.list.d/openjdk-r-ppa-trusty.list"
+readonly JDK_PPA="ppa:openjdk-r/ppa"
+readonly NETB_PPA_F="/etc/apt/sources.list.d/vajdics-netbeans-installer-trusty.list"
+readonly NETB_PPA="ppa:vajdics/netbeans-installer"
+readonly SUB3_PPA_F="/etc/apt/sources.list.d/webupd8team-sublime-text-3-trusty.list"
+readonly SUB3_PPA="ppa:webupd8team/sublime-text-3"
 
 # Detectar Sistema Operacional
 
-echo "[Script] Sistema Operacional detectado: $DESCRIPTION ($ARQPROC-bit)"
+echo "[Script] Sistema Operacional detectado: $DESCRIPTION ($ARQ_PROC-bit)"
 
 if [ $DISTRIBUTOR != "LinuxMint" ] || [ $CODENAME != "rosa" ] || \
-	[ $ARQPROC -ne 32 ]
+	[ $ARQ_PROC -ne 32 ]
 then
 	echo "[Script] Esse script foi escrito para Linux Mint 17.3 Rosa (32-bit)"
 	echo "[Script] O sistema é incompatível!"
@@ -59,7 +62,7 @@ printf "[Script] OK...\n\n"
 # Atualizar pacotes
 
 echo "[Script] Atualizando repositórios da distribuição..."
-# apt-get update
+ apt-get update
 printf "[Script] OK...\n\n"
 
 echo "[Script] Atualizando os pacotes..."
@@ -69,105 +72,177 @@ printf "[Script] OK...\n\n"
 # Codecs e ferramentas de compilador
 
 echo "[Script] Instalando codecs e ferramentas de compilador..."
-sudo apt-get install ubuntu-restricted-extras build-essential -y
+apt-get install ubuntu-restricted-extras build-essential -y
 printf "[Script] OK...\n\n"
 
 # Java OpenJDK 8
 
-if [ ! -s "$JDKPPA" ]; then
+if [ ! -s "$JDK_PPA_F" ]; then
 	echo "[Script] Adicionando repositório de terceiros..."
-	sudo apt-add-repository ppa:openjdk-r/ppa -y
+	apt-add-repository $JDK_PPA -y
 	printf "[Script] OK...\n\n"
 
 	echo "[Script] Atualizando caché do repositório..."
-	sudo apt-get update
+	apt-get update
 	printf "[Script] OK...\n\n"
 else
-	echo "[Script] ppa:openjdk-r/ppa já existe"
+	echo "[Script] $JDK_PPA já existe"
 	printf "[Script] OK...\n\n"
 fi
 
 echo "[Script] Instalando Java OpenJDK 8..."
-sudo apt-get install openjdk-8-jdk -y
+apt-get install openjdk-8-jdk -y
 printf "[Script] OK...\n\n"
 
-exit 0
+echo "[Script] Selecionando a versão 8 do OpenJDK..."
+echo "2" | update-alternatives --config java
+printf "[Script] OK...\n\n"
 
-echo 'Por favor, selecione a versão 8 do Java SDK:'
-sudo update-alternatives --config java
-sudo update-alternatives --config javac
+echo "[Script] Verificando versão OpenJDK..."
+java -version
+javac -version
+printf "[Script] OK...\n\n"
 
-echo '######################################'
-echo '############ Netbeans 8.1 ############'
-echo '######################################'
+# Netbeans IDE 8.1
 
-sudo add-apt-repository ppa:vajdics/netbeans-installer -y
-sudo apt-get update
-sudo apt-get install netbeans-installer -y
+if [ ! -s "$NETB_PPA_F" ]; then
+	echo "[Script] Adicionando repositório de terceiros..."
+	add-apt-repository $NETB_PPA -y
+	printf "[Script] OK...\n\n"
 
-echo '########################################'
-echo '############ Sublime Text 3 ############'
-echo '########################################'
+	echo "[Script] Atualizando caché do repositório..."
+	apt-get update
+	printf "[Script] OK...\n\n"
+else
+	echo "[Script] $NETB_PPA já existe"
+	printf "[Script] OK...\n\n"
+fi
 
-sudo add-apt-repository ppa:webupd8team/sublime-text-3 -y
-sudo apt-get update
-sudo apt-get install sublime-text -y
+echo "[Script] Instalando Netbeans 8.1 IDE..."
+apt-get install netbeans-installer -y
+printf "[Script] OK...\n\n"
 
-echo '######################################'
-echo '############ Install Apps ############'
-echo '######################################'
+# Sublime Text
 
-sudo apt-get install git vim inkscape dia meld mysql-workbench pyrenamer -y
+if [ ! -s "$SUB3_PPA_F" ]; then
+	echo "[Script] Adicionando repositório de terceiros..."
+	add-apt-repository $SUB3_PPA -y
+	printf "[Script] OK...\n\n"
 
-echo '################################'
-echo '############ Apache ############'
-echo '################################'
+	echo "[Script] Atualizando caché do repositório..."
+	apt-get update
+	printf "[Script] OK...\n\n"
+else
+	echo "[Script] $SUB3_PPA já existe"
+	printf "[Script] OK...\n\n"
+fi
 
-sudo apt-get install apache2 -y
-sudo chown -R $USER.www-data /var/www/html
-sudo chmod -R 775 /var/www/html
-ln -s /var/www/html ~
+echo "[Script] Instalando Sublime Text 3..."
+apt-get install sublime-text -y
+printf "[Script] OK...\n\n"
 
-echo '#############################'
-echo '############ PHP ############'
-echo '#############################'
+# Aplicativos
 
-sudo apt-get install php5 libapache2-mod-php5 -y
-echo '<?php phpinfo(); ?>' > ~/html/testphp.php
+echo "[Script] Instalando Aplicativos:"
+echo "[Script] - git (Controle de versões)"
+apt-get install git -y
+echo "[Script] - vim (Editor de texto)"
+apt-get install vim -y
+echo "[Script] - inkscape (Disenho vetorial)"
+apt-get install inkscape -y
+echo "[Script] - dia (Diagramas)"
+apt-get install dia -y
+echo "[Script] - meld (Comparador de arquivos)"
+apt-get install meld -y
+echo "[Script] - mysql-workbench (Administração de DB)"
+apt-get install mysql-workbench -y
+echo "[Script] - pyrenamer (Renomear arquivos em lote)"
+apt-get install pyrenamer -y
+printf "[Script] OK...\n\n"
 
-echo '###############################'
-echo '############ MySQL ############'
-echo '###############################'
+# LAMP - Apache
 
-sudo apt-get install mysql-server -y
-sudo apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin -y
-sudo sed -i '/;\s*extension=mysql\.so/s/;\s*//' /etc/php5/apache2/php.ini
-sudo /etc/init.d/apache2 restart
+echo "[Script] Instalando servidor Apache..."
+apt-get install apache2 -y
+printf "[Script] OK...\n\n"
 
-echo '####################################'
-echo '############ phpMyAdmin ############'
-echo '####################################'
+echo "[Script] Mudando as permisões de /var/www/html..."
+chown -R $USER.www-data /var/www/html
+chmod -R 775 /var/www/html
+printf "[Script] OK...\n\n"
 
-sudo sed -i -E '/^\s*(\/){2}\s*.*AllowNoPassword/s/^\s*(\/){2}\s*//' /etc/phpmyadmin/config.inc.php
+echo "[Script] Criando link simbólico na home..."
+ln -s /var/www/html /home/$USER
+printf "[Script] OK...\n\n"
+
+# LAMP - PHP
+
+echo "[Script] Instalando linguagem de programação PHP..."
+apt-get install php5 libapache2-mod-php5 -y
+printf "[Script] OK...\n\n"
+
+# LAMP - MySQL
+
+echo "[Script] Instalando banco de dados MySQL..."
+apt-get install mysql-server -y
+apt-get install libapache2-mod-auth-mysql php5-mysql phpmyadmin -y
+printf "[Script] OK...\n\n"
+
+echo "[Script] Configurando PHP para trabalhar com MySQL..."
+
+if [ ! -s "/etc/php5/apache2/php.ini.bak" ]; then
+	# Backup
+	cp /etc/php5/apache2/php.ini /etc/php5/apache2/php.ini.bak
+fi
+
+sed -i '/;\s*extension=mysql\.so/s/;\s*//' /etc/php5/apache2/php.ini
+printf "[Script] OK...\n\n"
+
+echo "[Script] Reiniciando Apache..."
+/etc/init.d/apache2 restart
+printf "[Script] OK...\n\n"
+
+# LAMP - phpMyAdmin
+
+echo "[Script] Configurando phpMyAdmin para acessar sem senha..."
+
+if [ ! -s "/etc/phpmyadmin/config.inc.php.bak" ]; then
+	# Backup
+	cp /etc/phpmyadmin/config.inc.php /etc/phpmyadmin/config.inc.php.bak
+fi
+
+sed -i -E '/^\s*(\/){2}\s*.*AllowNoPassword/s/^\s*(\/){2}\s*//' /etc/phpmyadmin/config.inc.php
+printf "[Script] OK...\n\n"
+
+echo "[Script] Configurando Apache para acessar phpMyAdmin..."
+
+if [ ! -s "/etc/apache2/apache2.conf.bak" ]; then
+	# Backup
+	cp /etc/apache2/apache2.conf /etc/apache2/apache2.conf.bak
+fi
 sudo echo 'Include /etc/phpmyadmin/apache.conf' >> /etc/apache2/apache2.conf
-sudo /etc/init.d/apache2 restart
+printf "[Script] OK...\n\n"
 
-echo '#######################################'
-echo '############ Conta "aluno" ############'
-echo '#######################################'
+echo "[Script] Reiniciando Apache..."
+/etc/init.d/apache2 restart
+printf "[Script] OK...\n\n"
 
-echo 'Criando conta aluno...'
-echo 'password: "aluno". Ignorar as outras opções.'
-sudo adduser aluno
-sudo adduser aluno www-data
-sudo ln -s /var/www/html /home/aluno
-sudo inxi -F > ~/hardinfo.txt
+# Conta Aluno
+
+echo "[Script] Criando conta aluno (passwd: 'aluno'...)"
+adduser aluno
+adduser aluno www-data
+printf "[Script] OK...\n\n"
+
+echo "[Script] Criando link simbólico na home do aluno para /var/www/html..."
+ln -s /var/www/html /home/aluno
+printf "[Script] OK...\n\n"
+
+echo "[Script] Criando arquivo de texto na home com especificações do hardware..."
+inxi -F > /home/$USER/hardinfo.txt
+printf "[Script] OK...\n\n"
 
 # Testes
-
-echo '####################################'
-echo '############ CHECKLIST #############'
-echo '####################################'
 
 echo 'Verifique as seguintes PPAs (não duplicadas):'
 echo '============================================='
@@ -175,9 +250,9 @@ echo '   - ppa:openjdk-r/ppa'
 echo '   - ppa:vajdics/netbeans-installer'
 echo '   - ppa:webupd8team/sublime-text-3'
 read -p 'Enter para continuar: '
-sudo inxi -r | grep openjdk-r
-sudo inxi -r | grep netbeans-installer
-sudo inxi -r | grep webupd8team
+inxi -r | grep openjdk-r
+inxi -r | grep netbeans-installer
+inxi -r | grep webupd8team
 read -p 'Enter para continuar: '
 
 echo 'Verifique a versão do Java:'
@@ -186,20 +261,29 @@ java -version
 javac -version
 read -p 'Enter para continuar: '
 
-echo 'Verifique se os seguentes softwares foram instalado e os mesmos'
-echo '==============================================================='
-echo 'estão funcionando corretamente:'
-echo '==============================='
-echo '   - Netbeans 8.1 (Plugins PHP e HTML)'
-echo '   - Workbench'
-echo '   - Inkscape'
-echo '   - Gimp'
-echo '   - Sublime-Text 3'
-echo '   - Meld'
-echo '   - PyRenamer'
-echo '   - Dia'
-echo '   - Vim'
-echo '   - Git'
+echo 'Verifique se os seguentes softwares foram instalado e os mesmos estão funcionando corretamente:'
+echo '==============================================================================================='
+printf '   - Workbench: '
+mysql-workbench --version
+printf '   - Inkscape: '
+inkscape --version
+printf '   - Gimp: '
+gimp --version
+printf '   - Sublime-Text 3: '
+sublime-text --version
+printf '   - Meld: '
+meld --version
+printf '   - PyRenamer: '
+pyrenamer --version
+printf '   - Dia: '
+dia --version
+printf '   - Vim: '
+vim --version
+printf '   - Git: '
+git --version
+echo '   - Netbeans (manual: versão, atualizações, plugins)'
+netbeans --nosplash &
+
 read -p 'Enter para continuar: '
 
 echo 'Verifique se a conta "aluno" foi criada corretamente:'
@@ -209,7 +293,7 @@ cat /etc/passwd | grep aluno
 echo 'groups:'
 groups aluno
 echo 'link simbólico html (Apache):'
-ls -l ~/html
+ls -l /home/aluno/html
 echo 'permissões:'
 ls -ld /var/www/html
 read -p 'Enter para continuar: '
@@ -220,10 +304,10 @@ echo '- Apache'
 echo '- PHP'
 echo '- MySQL e phpMyAdmin'
 echo '===================='
+echo '<?php phpinfo(); ?>' > /home/$USER/html/testphp.php
 firefox http://localhost/ http://localhost/testphp.php http://localhost/phpmyadmin 2>/dev/null &
 read -p 'Enter para continuar: '
-
-sudo rm ~/html/testphp.php
+rm /home/$USER/html/testphp.php
 
 echo 'Verifique os arquivos de configuração:'
 echo '======================================'
@@ -235,13 +319,12 @@ echo '/etc/php5/apache2/php.ini'
 cat /etc/php5/apache2/php.ini | grep 'extension=mysql.so/extension=mysql.so'
 read -p 'Enter para continuar: '
 
-echo 'Verifique se o Google é o search engine pro Firefox'
-echo '==================================================='
-echo 'nas contas "etec" e "aluno"'
-echo '==========================='
+echo 'Verifique se o Google é o search engine pro Firefox nas contas "etec" e "aluno"'
+echo '==============================================================================='
 read -p 'Enter para continuar: '
 
 echo 'Verifique se o relatório de hardware foi criado na home:'
 echo '========================================================'
-cat ~/hardinfo.txt
+cat /home/$USER/hardinfo.txt
 read -p 'Enter para continuar: '
+
